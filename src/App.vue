f9eadaabb3d1467bef4b0c37cc26fa70914b312d
Rimon <rimons2804@gmail.com>
2022-09-04 19:24:14 +0300

    burger menu added to header

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,13 @@
   <section class="main-layout">
     <div class="full">
       <app-header />
-
     </div>
     <item-list />
   </section>
 </template>
 
 <script>
-import appHeader from '@/cmps/app-header.vue'
+import appHeader from '@/cmps/header.vue'
 import appFooter from './cmps/app-footer.vue'
 import itemList from './cmps/item-list.vue'
 export default {
---
+++ src/cmps/header.vue
@@ -0,0 +1,35 @@
+<template>
+  <section class="main-header">
+    <div class="content">
+      <div class="logo">logo</div>
+
+      <ul :class='{show: isMenuOpen}'>
+        <li>Home</li>
+        <li>Gallery</li>
+        <li>Contact</li>
+      </ul>
+
+      <div class="burger fa-solid" @click="toggleMenu"></div>
+    </div>
+  </section>
+</template>
+  <script>
+  export default {
+    name: 'ProjectApp',
+    components: {},
+    data() {
+      return {
+        isMenuOpen:false
+      };
+    },
+    created() { },
+    methods: {
+      toggleMenu(){
+        this.isMenuOpen = !this.isMenuOpen
+      }
+    },
+    computed: {},
+    unmounted() { },
+  };
+  
+  </script>
