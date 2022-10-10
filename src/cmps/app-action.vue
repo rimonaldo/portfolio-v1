e580fa51b16d49ad1a447008e7fa46e53864df16
Rimon <rimons2804@gmail.com>
2022-10-11 00:35:20 +0300

    footer work

--- src/cmps/app-action.vue
+++ src/cmps/app-action.vue
@@ -11,7 +11,7 @@
       <form ref="form" @submit.prevent="sendEmail">
         <div class="top">
           <input class="email-input" v-model="mail" type="email" ref="msg" placeholder="email@example.com">
-          <button type="submit" class=" main-btn">Say Hello!</button>
+          <button  type="submit" class="submit-msg main-btn">Say Hello!</button>
         </div>
         <textarea name="message" id="" cols="30" rows="2" v-model="showMail"></textarea>
       </form>
@@ -33,12 +33,14 @@ export default {
   created() { },
   methods: {
     sendEmail() {
+      const elSub = document.querySelector('.submit-msg')
       emailjs.sendForm('service_yz0s5jo', 'template_mfa7hl5', this.$refs.form, 'okVPqSb_o_u3PnW-1')
-        .then((result) => {
+      .then((result) => {
+          elSub.innerText = 'Message sent! '
           console.log('SUCCESS!', result.text);
         }, (error) => {
           console.log('FAILED...', error.text);
-        });
+        }); 
     }
   },
   computed: {
